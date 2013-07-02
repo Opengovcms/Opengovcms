f3b4d44a5275cb839d5ce82750911b214805653e
Sune Broendum Woeller <sune@woeller.dk>
2013-07-02 17:12:02 +0200

    show top level menu items in ctrl panel

--- src/policy/policy/browser/controlpanel.py
+++ src/policy/policy/browser/controlpanel.py
@@ -1,11 +1,15 @@
 from policy.i18n import MessageFactory as _
 from policy.interfaces import ISiteSettings
 from zope.interface import Interface
+from zope.component import getMultiAdapter
 from zope import schema
+from Acquisition import aq_inner
 
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone.app.registry.browser import controlpanel
 from plone.app.z3cform.wysiwyg import WysiwygFieldWidget
 from plone.z3cform import layout
+from plone.z3cform.layout import FormWrapper
 
 from z3c.form import field
 
@@ -29,6 +33,7 @@ ControlPanel = layout.wrap_form(
 
 class IFlyoutSettings(Interface):
     """ Fly out menu settings """
+    # so far no settings here.
 
 
 class FlyoutControlPanelEditForm(controlpanel.RegistryEditForm):
@@ -41,8 +46,13 @@ class FlyoutControlPanelEditForm(controlpanel.RegistryEditForm):
     )
 
 
-FlyoutControlPanel = layout.wrap_form(
-    FlyoutControlPanelEditForm,
-    controlpanel.ControlPanelFormWrapper
-)
-# todo: set index
+class FlyoutControlPanel(FormWrapper):
+
+    form = FlyoutControlPanelEditForm
+    index = ViewPageTemplateFile('templates/controlpanel_flyout.pt')
+
+    def top_menu_items(self):
+        context = aq_inner(self.context)
+        portal_tabs_view = getMultiAdapter((context, self.request),
+                                           name='portal_tabs_view')
+        return portal_tabs_view.topLevelTabs()

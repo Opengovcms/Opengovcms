d3bccc1fa90d457199cb549caa834de15172e58e
Roman Kozlovskyi <kroman0@quintagroup.com>
2013-07-02 12:58:49 +0000

    Added viewlet for megamenu

---
+++ src/policy/policy/browser/sections.py
@@ -0,0 +1,27 @@
+from plone.app.layout.viewlets.common import GlobalSectionsViewlet as ViewletBase
+from plone.app.layout.navigation.defaultpage import getDefaultPage
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from Acquisition import aq_inner
+from zope.component.hooks import getSite
+
+from collective.panels.traversal import PanelManager
+
+
+class GlobalSectionsViewlet(ViewletBase):
+    index = ViewPageTemplateFile('templates/sections.pt')
+
+    def getItem(self, tab):
+        site = getSite()
+        context = aq_inner(self.context)
+        if tab.get('available'):
+            item = site
+        else:
+            item = site.restrictedTraverse(tab['id'])
+        def_page = getDefaultPage(item)
+        if def_page:
+            item = item.restrictedTraverse(def_page)
+        return item
+
+    def getPanels(self, context):
+        manager = PanelManager(context, self.request, context, "plone.portaltop").__of__(context)
+        return tuple(manager)

4dab824fe2f0e5405b011d52716d484db31b3643
Sune Broendum Woeller <sune@woeller.dk>
2013-07-02 16:12:12 +0200

    add empty control panle for fly out menu

--- src/policy/policy/browser/controlpanel.py
+++ src/policy/policy/browser/controlpanel.py
@@ -1,5 +1,7 @@
 from policy.i18n import MessageFactory as _
 from policy.interfaces import ISiteSettings
+from zope.interface import Interface
+from zope import schema
 
 from plone.app.registry.browser import controlpanel
 from plone.app.z3cform.wysiwyg import WysiwygFieldWidget
@@ -23,3 +25,24 @@ ControlPanel = layout.wrap_form(
     ControlPanelEditForm,
     controlpanel.ControlPanelFormWrapper
 )
+
+
+class IFlyoutSettings(Interface):
+    """ Fly out menu settings """
+
+
+class FlyoutControlPanelEditForm(controlpanel.RegistryEditForm):
+    schema = IFlyoutSettings
+    fields = field.Fields(IFlyoutSettings)
+
+    label = _(u"Configure fly-out menu")
+    description = _(
+        u"Here you can edit the content of the fly-out menu."
+    )
+
+
+FlyoutControlPanel = layout.wrap_form(
+    FlyoutControlPanelEditForm,
+    controlpanel.ControlPanelFormWrapper
+)
+# todo: set index

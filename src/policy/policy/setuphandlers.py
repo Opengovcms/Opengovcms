bef1214535529b4410f1c3a09e008cbd08a4bc10
Roman Kozlovskyi <kroman0@quintagroup.com>
2013-07-04 16:42:11 +0000

    Hide profiles and products

--- src/policy/policy/setuphandlers.py
+++ src/policy/policy/setuphandlers.py
@@ -1,6 +1,58 @@
+from Products.CMFQuickInstallerTool.interfaces import INonInstallable
+from plone.app.contenttypes.migration import migration
 from zope.component import getMultiAdapter
 from zope.component.hooks import getSite
-from plone.app.contenttypes.migration import migration
+from zope.interface import implements
+
+
+class HiddenProducts(object):
+    implements(INonInstallable)
+
+    def getNonInstallableProducts(self):
+        return [
+            'PloneFormGen',
+            'Products.PloneFormGen',
+            'plone.app.dexterity',
+            'plone.formwidget.autocomplete',
+            'plone.formwidget.contenttree',
+            'plone.app.contenttypes',
+            'plone.app.theming',
+            'plone.app.caching',
+            'plone.app.imagecropping',
+            'plone.app.relationfield',
+            'collective.z3cform.datetimewidget',
+            'collective.panels',
+            'windowZ',
+            'Products.windowZ',
+            'collective.portlet.rich',
+            'quintagroup.portlet.generichtml',
+            'collective.portlet.ngcollection',
+            'collective.portlet.carousel',
+            'collective.portletmetadata',
+            'smtemplate.theme',
+            ]
+
+    def getNonInstallableProfiles(self):
+        return [
+            'Products.PloneFormGen:default',
+            'plone.app.dexterity:default',
+            'plone.formwidget.autocomplete',
+            'plone.formwidget.contenttree',
+            'plone.app.contenttypes:default',
+            'plone.app.theming:default',
+            'plone.app.caching:default',
+            'plone.app.imagecropping:default',
+            'plone.app.relationfield:default',
+            'collective.z3cform.datetimewidget:default',
+            'collective.panels:default',
+            'Products.windowZ:default',
+            'collective.portlet.rich:default',
+            'quintagroup.portlet.generichtml:default',
+            'collective.portlet.ngcollection:default',
+            'collective.portlet.carousel:default',
+            'collective.portletmetadata:default',
+            'smtemplate.theme:default',
+            ]
 
 def migrateAT2DX(context):
     """

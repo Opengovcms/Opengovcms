3aa0f4a439cdf707a618ca9635d1379cf5ce509c
Roman Kozlovskyi <kroman0@quintagroup.com>
2013-07-08 13:36:46 +0000

    Show panels only for diazo theme

--- src/policy/policy/browser/sections.py
+++ src/policy/policy/browser/sections.py
@@ -5,10 +5,15 @@ from Acquisition import aq_inner
 from zope.component.hooks import getSite
 
 from collective.panels.traversal import PanelManager
+from plone.app.theming.utils import isThemeEnabled
 
 
 class GlobalSectionsViewlet(ViewletBase):
     index = ViewPageTemplateFile('templates/sections.pt')
+
+    @property
+    def isThemeEnabled(self):
+        return isThemeEnabled(self.request)
 
     def getItem(self, tab):
         site = getSite()

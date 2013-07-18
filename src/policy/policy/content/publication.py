79bc0f7c5defd419e2ac79efecd0112070e66a05
Sune Broendum Woeller <sune@woeller.dk>
2013-07-18 13:23:31 +0200

    publication changes

--- src/policy/policy/content/publication.py
+++ src/policy/policy/content/publication.py
@@ -1,18 +1,13 @@
 # -*- coding: utf-8 -*-
-from zope import schema
-
-from plone.namedfile.field import NamedBlobImage, NamedBlobFile
 from plone.directives import form
 from plone.app.textfield import RichText
 from collective import dexteritytextindexer
 
-from policy import validators
 from policy.i18n import MessageFactory as _
 from Products.CMFPlone import PloneMessageFactory as _pmf
-from plone.app.imagecropping.interfaces import IImageCropping
 
 
-class IPublication(form.Schema, IImageCropping):
+class IPublication(form.Schema):
     """ Publication """
 
     dexteritytextindexer.searchable('text')

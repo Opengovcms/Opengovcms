37c3f4c04519d4a966596b708d591b1c3bfcb8d3
Roman Kozlovskyi <kroman0@quintagroup.com>
2013-07-23 13:40:11 +0000

    sections search

--- src/policy/policy/browser/search.py
+++ src/policy/policy/browser/search.py
@@ -1,7 +1,67 @@
-from plone.app.search.browser import Search as SearchBase
+from plone.app.search.browser import Search as SearchBase, MULTISPACE, EVER, quote_chars
 from Products.CMFCore.utils import getToolByName
+from Products.CMFPlone.browser.navtree import getNavigationRoot
+
+
+TYPES = ['Document', 'Event', 'News Item']
+
 
 class Search(SearchBase):
 
-    def filter_types(self, types):
-        return ['Document', 'Event', 'News Item']
+    def types_list(self):
+        # only show those types that have any content
+        catalog = getToolByName(self.context, 'portal_catalog')
+        used_types = catalog._catalog.getIndex('portal_type').uniqueValues()
+        used_types = [i for i in used_types if i in TYPES]
+        return self.filter_types(list(used_types))
+
+    def filter_sections(self, sections):
+        return [i for i in sections if not i.exclude_from_nav]
+
+    def sections_list(self):
+        catalog = getToolByName(self.context, 'portal_catalog')
+        path = '/'.join(self.context.getPhysicalPath())
+        sections = catalog(portal_type="Folder", path={'query': path, 'depth': 1})
+        return self.filter_sections(sections)
+
+    def filter_query(self, query):
+        request = self.request
+
+        catalog = getToolByName(self.context, 'portal_catalog')
+        valid_indexes = tuple(catalog.indexes())
+        valid_keys = self.valid_keys + valid_indexes
+
+        text = query.get('SearchableText', None)
+        if text is None:
+            text = request.form.get('SearchableText', '')
+        if not text:
+            # Without text, must provide a meaningful non-empty search
+            valid = set(valid_indexes).intersection(request.form.keys())
+            if not valid:
+                return
+
+        for k, v in request.form.items():
+            if v and ((k in valid_keys) or k.startswith('facet.')):
+                query[k] = v
+        if text:
+            query['SearchableText'] = quote_chars(text)
+
+        # don't filter on created at all if we want all results
+        created = query.get('created')
+        if created:
+            if created.get('query'):
+                if created['query'][0] <= EVER:
+                    del query['created']
+
+        # respect `types_not_searched` setting
+        types = query.get('portal_type', list(catalog._catalog.getIndex('portal_type').uniqueValues()))
+        if 'query' in types:
+            types = types['query']
+        query['portal_type'] = self.filter_types(types)
+        # respect effective/expiration date
+        query['show_inactive'] = False
+        # respect navigation root
+        if 'path' not in query:
+            query['path'] = getNavigationRoot(self.context)
+
+        return query

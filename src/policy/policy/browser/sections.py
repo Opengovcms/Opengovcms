282b894dec498574d77e9c6d40e4d9f50ec408da
Roman Kozlovskyi <kroman0@quintagroup.com>
2013-08-13 10:06:53 +0000

    Added document_byline to plone.belowcontentbody

--- src/policy/policy/browser/sections.py
+++ src/policy/policy/browser/sections.py
@@ -1,4 +1,5 @@
 from plone.app.layout.viewlets.common import GlobalSectionsViewlet as ViewletBase
+from plone.app.layout.viewlets.content import DocumentBylineViewlet as BylineBase
 from plone.app.layout.navigation.defaultpage import getDefaultPage
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from Acquisition import aq_inner
@@ -37,3 +38,20 @@ class GlobalSectionsViewlet(ViewletBase):
             return tuple(manager)
         except TypeError:
             return tuple()
+
+class DocumentBylineViewlet(BylineBase):
+    index = ViewPageTemplateFile("templates/document_byline.pt")
+
+    def show(self):
+        is_portal_root = self.context_state.is_portal_root()
+        context = aq_inner(self.context)
+        try:
+            manager = PanelManager(context,
+                                   self.request,
+                                   context,
+                                   "plone.belowcontentbody"
+                                   ).__of__(context)
+            panels = len(tuple(manager)) > 0
+        except:
+            panels = False
+        return not is_portal_root and not panels

b12fc22f31245f4f78c3c9f7b36e6418b25c2bea
Roman Kozlovskyi <kroman0@quintagroup.com>
2013-09-09 14:49:40 +0000

    Added 'Alt image text' behavior

---
+++ src/policy/policy/content/alt.py
@@ -0,0 +1,30 @@
+from z3c.form.interfaces import IEditForm, IAddForm
+from zope.interface import alsoProvides
+from zope import schema
+from plone.autoform.interfaces import IFormFieldProvider
+from plone.autoform import directives as form
+from plone.supermodel import model
+from policy.i18n import MessageFactory as _
+
+
+class IAltImageText(model.Schema):
+    """Behavior interface to set alt image text for content with image fields
+    """
+
+    alt_image_text = schema.TextLine(
+        title=_(
+            u'label_alt_image_text',
+            default=u'Alt image text'
+        ),
+        description=_(
+            u'help_alt_image_text',
+            default=u'Alt text for image'
+        ),
+        default=''
+    )
+
+    form.omitted('alt_image_text')
+    form.no_omit(IEditForm, 'alt_image_text')
+    form.no_omit(IAddForm, 'alt_image_text')
+
+alsoProvides(IAltImageText, IFormFieldProvider)

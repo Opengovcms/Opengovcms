5a0179c82c6bb4e85c021ec5a6eea584ee53ac1c
Sune Broendum Woeller <sune@woeller.dk>
2014-01-13 13:56:35 +0100

    update virtualenv bootstrap script and bootstrap script creation

--- create_virtualenv_bootstrap.py
+++ create_virtualenv_bootstrap.py
@@ -12,9 +12,12 @@ def after_install(options, home_dir):
     # Run script example:
     #subprocess.call([join(home_dir, bin, 'my-package-script'),
     #                 'setup', home_dir])
-    if os.path.exists('bootstrap.py'):
+     if not os.path.exists(os.path.join(home_dir, 'buildout.cfg')):
+         shutil.copy(os.path.join(home_dir, 'buildout.cfg.example'),
+                     os.path.join(home_dir, 'buildout.cfg'))
+    if os.path.exists('buildout-bootstrap.py'):
         logger.notify('Running buildout bootstrap')
         subprocess.call([os.path.join(home_dir, bin, 'python'),
-                        'bootstrap.py'])
+                        'buildout-bootstrap.py', '-v 2.2.1'])
 """))
 f = open('bootstrap_virtualenv_.py', 'w').write(output)
